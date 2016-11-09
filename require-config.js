fedf8bdeba5abbf38e47d4513a03c03b3f6e9f47
Gabriel Lupu <lupugabriel1@gmail.com>
2016-11-08 22:30:14 -0500

    update melvinjs require config

--- require-config.js
+++ require-config.js
@@ -1,21 +1,21 @@
 /*jshint camelcase: false */
 require.config({
-    paths: {
-        jquery: '/node_modules/jquery/dist/jquery.min',
-        underscore: '/node_modules/underscore/underscore-min',
-        backbone: '/node_modules/backbone/backbone-min',
-        'backbone.radio': '/node_modules/backbone.radio/build/backbone.radio',
-        marionette: '/node_modules/backbone.marionette/lib/backbone.marionette',
-        melvin: '/node_modules/melvinjs/lib/melvinjs',
-        bootstrap: '/node_modules/bootstrap/dist/js/bootstrap',
-        templates: '../templates'
+    "paths": {
+        "jquery": "../../node_modules/jquery/dist/jquery.min",
+        "underscore": "../../node_modules/underscore/underscore-min",
+        "backbone": "../../node_modules/backbone/backbone-min",
+        "backbone.radio": "../../node_modules/backbone.radio/build/backbone.radio",
+        "marionette": "../../node_modules/backbone.marionette/lib/backbone.marionette",
+        "melvin": "../../node_modules/melvinjs/lib/melvinjs",
+        "bootstrap": "../../node_modules/bootstrap/dist/js/bootstrap",
+        "templates": "../templates"
     },
-    shim: {
-        bootstrap: {
-            deps: ['jquery']
+    "shim": {
+        "bootstrap": {
+            "deps": ["jquery"]
         },
-        app: {
-            deps: ['melvin', 'bootstrap', 'templates']
+        "app": {
+            "deps": ["melvin", "bootstrap", "templates"]
         }
     }
 });
